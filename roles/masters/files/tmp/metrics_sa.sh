9da9153741cded75bae7be608c09248fbc86dbf4
Guillaume Coré <fridim@onfi.re>
2015-09-23 17:07:14 +0200

    create serviceaccount metrics, and deploy token

---
+++ roles/masters/files/tmp/metrics_sa.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# creates the metrics service account and the ansible local facts associated
+set -x
+set -e
+
+[ "root" = "$(whoami)" ]
+
+oc get serviceaccount || oc create -f - <<EOF
+{
+  "apiVersion": "v1",
+  "kind": "ServiceAccount",
+  "metadata": {
+    "name": "metrics"
+  }
+}
+EOF
+
+oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:default:metrics
+
+token_name=$(oc get serviceaccount metrics -o json | /opt/bin/jq -r '.secrets[].name | select(  contains( "dockercfg" ) | not)'|tail -n1)
+token=$(oc get secret ${token_name} -o json | /opt/bin/jq -r '.data.token')
+
+# add ansible fact
+mkdir -p /etc/ansible/facts.d/
+fact=/etc/ansible/facts.d/openshift_data.fact
+touch $fact
+chmod 600 $fact
+cat >>$fact <<EOF
+[tokens]
+metrics=${token}
+EOF
