6ba61b6d30e29f8711c917541b2a2d188d3dae09
talset <florian.lambert@enovance.com>
2015-10-22 18:59:23 +0200

    fix token decode base64

--- roles/masters/files/tmp/metrics_sa.sh
+++ roles/masters/files/tmp/metrics_sa.sh
@@ -18,7 +18,7 @@ EOF
 oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:default:metrics
 
 token_name=$(oc get serviceaccount metrics -o json | /opt/bin/jq -r '.secrets[].name | select(  contains( "dockercfg" ) | not)'|tail -n1)
-token=$(oc get secret ${token_name} -o json | /opt/bin/jq -r '.data.token')
+token=$(oc get secret ${token_name} -o json | /opt/bin/jq -r '.data.token' | base64 --decode)
 
 # add ansible fact
 mkdir -p /etc/ansible/facts.d/
