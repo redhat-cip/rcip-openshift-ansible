de654e46238f684f007307c0e7ce6feb65e9a41a
Guillaume Coré <gucore@redhat.com>
2016-04-26 12:39:07 +0200

    stabilize functional tests
    - don't run ansible playbook with default forks 5. Too much parallelism can
      kill performances on the slave VM
    - add log_collector playbook that will collect logs and conf on hosts
      if one of the playbooks fails
    - try to run playbooks several time before failing in order to avoid external
      issues as much as possible (like a yum repo not available temporarly).
    - increase ssh timeout in ansible.cfg
    
    Change-Id: I034b78b78daf2be498f7baa11e3d4d012e3082df

--- run_functional-tests.sh
+++ run_functional-tests.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+
+# number of fork in ansible run, level of parallelism.
+_fork=3
+# size in M of swap to create on the slave node
+_swap=10240
+# number of lines to collect on nodes/masters/.. if one of the playbooks fails.
+_loglines=10000
+# number of time to retry ansible-playbook run before failing
+_retry=3
+# version (tag) of the openshift/openshift-ansible repository
+_openshift_ansible_version=3.0.78-1
+
+# trap function to collect log if needed
+_on_exit() {
+  local exit_status=${1:-$?}
+  set +e
+
+  if [ ${exit_status} != 0 ]; then
+    ansible-playbook -i ci/hosts_centos_origin ci/log_collector.yml -e loglines=${_loglines}
+  fi
+
+  exit $exit_status
+}
+
+trap "_on_exit" EXIT
+
+
 echo "==== env ===="
 env
 
@@ -38,16 +65,16 @@ sudo sync
 
 # add some swap
 if [ ! -e /swapfile ]; then
-    sudo dd if=/dev/zero of=/swapfile bs=1M count=10240
+    sudo dd if=/dev/zero of=/swapfile bs=1M count=${_swap}
     sudo chmod 600 /swapfile
     sudo mkswap /swapfile
     sudo swapon /swapfile
 fi
 
-ansible-playbook -i ci/hosts_centos_origin ci/create_vm.yml
+ansible-playbook -f ${_fork} -i ci/hosts_centos_origin ci/create_vm.yml
 
 # RCIP pre
-ansible-playbook -i ci/hosts_centos_origin pre.yml
+ansible-playbook -f ${_fork} -i ci/hosts_centos_origin pre.yml
 
 # need ansible v1.9 for official playbook, see:
 # https://github.com/openshift/openshift-ansible/issues/1339
@@ -57,8 +84,27 @@ sudo yum install -y pyOpenSSL
 
 # official openshift ansible playbook
 git clone https://github.com/openshift/openshift-ansible.git
-(cd openshift-ansible && git checkout openshift-ansible-3.0.78-1)
-ansible-playbook -i ci/hosts_centos_origin openshift-ansible/playbooks/byo/config.yml
+(cd openshift-ansible && git checkout openshift-ansible-${_openshift_ansible_version})
+
+set +e
+
+# openshift-ansible
+for i in $(seq ${_retry}); do
+  ansible-playbook -f ${_fork} -i ci/hosts_centos_origin openshift-ansible/playbooks/byo/config.yml
+  RET=$?
+  [ $RET = 0 ] && break;
+done
+
+echo "byo/config.yml: ${i} tries"
+[ $RET = 0 ] || exit $RET
+
 
 # RCIP post
-ansible-playbook -i ci/hosts_centos_origin post.yml
+for j in $(seq ${_retry}); do
+  ansible-playbook -f ${_fork} -i ci/hosts_centos_origin post.yml
+  RET=$?
+  [ $RET = 0 ] && break;
+done
+
+echo "post.yml: ${j} tries"
+[ $RET = 0 ] || exit $RET
